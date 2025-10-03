69effc255a2d8c876406ef6d2c5d9adf496ac9d1
Pedro <pedro.fialho000@gmail.com>
2025-10-03 16:23:54 -0300

    refactor: alterando os diretórios

--- js/login.js
+++ js/login.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll(".option"); 
     const container = document.querySelector("main .side.aluno"); 
-    const selectOP = document.getElementById("select-op")
+    const selectOP = document.getElementById("select-op");
+    const loginBtn = document.getElementById("loginBtn");
+
+    let userType = 'aluno';
     buttons.forEach(button => {
         button.addEventListener("click", () => {
             
@@ -13,14 +16,21 @@ document.addEventListener("DOMContentLoaded", () => {
             if (button.classList.contains("aluno")) {
                 container.classList.add("aluno");
                 selectOP.textContent = "aluno";
+                userType = 'aluno';
             } else if (button.classList.contains("professor")) {
                 container.classList.add("professor");
                 selectOP.textContent = "professor";
+                userType = 'professor';
             } else if (button.classList.contains("admin")) {
                 container.classList.add("admin");
                 selectOP.textContent = "administrador";
+                userType = 'admin';
             }
         });
     });
+
+    loginBtn.addEventListener("click", ()=>{
+         window.location.href = `/views/${userType}/home.html`    
+    })
 });
 
